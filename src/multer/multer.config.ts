2d2d570f6733c60815d29cf28fbf75a8013367f8
Slegart <onur.akin@ug.bilkent.edu.tr>
2024-03-10 09:21:10 +0300

    multer config

--- src/multer/multer.config.ts
+++ src/multer/multer.config.ts
@@ -5,6 +5,7 @@ export const multerConfig = {
   storage: diskStorage({
     destination: (req, file, callback) => {
       const mimetype = file.mimetype.split('/')[1];
+      console.log('mimetype:', mimetype);
 
       if (!fs.existsSync('uploads')) {
         fs.mkdirSync('uploads', { recursive: true });
@@ -23,24 +24,38 @@ export const multerConfig = {
         callback(null, 'uploads/images');
       } else if (mimetype === 'mp4' || mimetype === 'mov') {
         callback(null, 'uploads/videos');
-      } else {
+      } else if (
+        mimetype === 'pdf' ||
+        mimetype === 'docx' ||
+        mimetype === 'txt' ||
+        mimetype === 'xlsx' ||
+        mimetype === 'pptx' ||
+        mimetype === 'csv' ||
+        mimetype === 'zip' ||
+        mimetype === 'rar' ||
+        mimetype === '7z' ||
+        mimetype === 'tar' ||
+        mimetype === 'gz' ||
+        mimetype === 'bz2' ||
+        mimetype === 'xz' ||
+        mimetype === 'xml') {
         callback(null, 'uploads/other');
       }
     },
-      filename: (req, file, callback) => {
-        const currentDate = new Date();
-        const day = currentDate.getDate().toString().padStart(2, '0');
-        const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
-        const year = currentDate.getFullYear();
-        const hours = currentDate.getHours().toString().padStart(2, '0');
-        const minutes = currentDate.getMinutes().toString().padStart(2, '0');
-        const seconds = currentDate.getSeconds().toString().padStart(2, '0');
-  
-        const uniqueSuffix = '-' + Math.round(Math.random() * 1e9) + '.' + file.mimetype.split('/')[1];
-        const formattedDate = `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
-        const fullFilename = `${formattedDate}${uniqueSuffix}`;
-  
-        callback(null, fullFilename); 
-      },
-    }),
-  }; 
+    filename: (req, file, callback) => {
+      const currentDate = new Date();
+      const day = currentDate.getDate().toString().padStart(2, '0');
+      const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+      const year = currentDate.getFullYear();
+      const hours = currentDate.getHours().toString().padStart(2, '0');
+      const minutes = currentDate.getMinutes().toString().padStart(2, '0');
+      const seconds = currentDate.getSeconds().toString().padStart(2, '0');
+
+      const uniqueSuffix = '-' + Math.round(Math.random() * 1e9) + '.' + file.mimetype.split('/')[1];
+      const formattedDate = `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
+      const fullFilename = `${formattedDate}${uniqueSuffix}`;
+
+      callback(null, fullFilename);
+    },
+  }),
+}; 
